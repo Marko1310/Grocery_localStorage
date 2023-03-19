e06657b6f2869ad2a3837d5cd3746fc28923b5e1
Marko1310 <marko.cabo13@gmail.com>
2023-03-19 10:42:34 +0100

    wrap app with  global context

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -3,7 +3,21 @@ import React, { createContext, useState } from "react";
 export const GlobalContext = createContext(null);
 
 export const ContexWrapper = (props) => {
+  // state for list of groceries
+  const [grocerieList, setGrocerieList] = useState(() => {
+    return JSON.parse(localStorage.getItem("groceries")) || [];
+  });
+
+  // state for main input field
+  const [input, setInput] = useState("");
+
+  // state for edit field
+  const [inputEdit, setInputEdit] = useState("");
+
+  // state when the product is being edited
   const [edit, setEdit] = useState(false);
+
+  // alert pop up
   const [alert, setAlert] = useState(false);
 
   return (
