5e9d27b43c1b6684b51ea1e89016a2bf23b55a0d
Marko1310 <marko.cabo13@gmail.com>
2023-03-19 10:16:13 +0100

    wrap app with  global context

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -4,8 +4,10 @@ export const GlobalContext = createContext(null);
 
 export const ContexWrapper = (props) => {
   const [edit, setEdit] = useState(false);
+  const [alert, setAlert] = useState(false);
+
   return (
-    <GlobalContext.Provider value={{ edit, setEdit }}>
+    <GlobalContext.Provider value={{ edit, setEdit, alert, setAlert }}>
       {props.children}
     </GlobalContext.Provider>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { ContexWrapper } from "./context/GlobalContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContexWrapper>
+      <App />
+    </ContexWrapper>
   </React.StrictMode>
 );
